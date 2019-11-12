eca295978b8b0307e3e990ac7b2e62616a75e9bd
Petr Snobelt <petr.snobelt@kiwi.com>
2019-11-12 16:05:47 +0100

    allow export of some dynamic routing pages

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/1': { page: '[no]', query: { title: 'hello-nextjs 1', no: 1 } },
+      '/2': { page: '[no]', query: { title: 'hello-nextjs 2', no: 2 } },
+      '/42': { page: '[no]', query: { title: 'hello-nextjs 42', no: 42 } },
+    }
+  },
+}
